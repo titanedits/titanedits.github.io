f11bf94fcd031062afa242972f2dee4ea3bc6489
StevenDahFish <stevendahfish@gmail.com>
2023-05-27 20:08:28 -0700

    Created Past Clients and Improved Mobile Support

--- static/js/core.js
+++ static/js/core.js
@@ -11,47 +11,90 @@ fetch('/static/json/videos.json')
 	.then(data => {
 		console.log(data)
 		data.forEach(item => {
-			const section = template.cloneNode(true);
-			console.log(section)
-			const sectionTitle = section.querySelector(".section_title")
-			const sectionDescription = section.querySelector(".section_description")
-			const iframeContainer = section.querySelector(".iframe_container")
-			
-			sections.appendChild(section);
-			sectionTitle.innerText = item.title
-			sectionDescription.innerText = item.description
-			
-			if (item.hasCoolFont == true) {
-				sectionTitle.classList.add("coolFont");
-			}
-			if (item.color != undefined) {
-				section.style["background-color"] = item.color;
-			}
-			if (item.descriptionColor != undefined) {
-				sectionDescription.style.color = item.descriptionColor;
-			}
+			if (item.special == "PAST_CLIENTS") {
+				const section = template.cloneNode(true);
+				const sectionTitle = section.querySelector(".section_title")
+				const sectionDescription = section.querySelector(".section_description")
+				const pastClients = section.querySelector(".iframe_container")
+				
+				section.id = "pastClients"
+				sections.appendChild(section);
 
-			let x = 789;
-			let y = 442;
+				sectionTitle.innerText = "Past Clients"
+				sectionDescription.innerText = "These are some of the past clients that I've worked with!"
+				pastClients.classList.add("past_clients_container")
 
-			if (item.videoLinks.length > 1) {
-				x = 640;
-				y = 360;
-			}
-			item.videoLinks.forEach(link => {
-				const iframe = document.createElement("iframe")
-				iframe.src = link[0]
-				iframe.setAttribute("frameBorder", "0")
-				iframe.setAttribute("allowfullscreen", "")
-				if (link[1] == "VIDEO") {
-					iframe.width = x
-					iframe.height = y
-				} else if (link[1] == "SHORT") {
-					iframe.width = y
-					iframe.height = x
+				// Past Clients
+				const pastClientTemplate = document.getElementById("pastClientTemplate").content.querySelector(".past_client");
+				fetch('/static/json/clients.json')
+					.then(response => {
+						if (!response.ok) {
+							throw new Error('Network response was not ok');
+						}
+						return response.json();
+					})
+					.then(data => {
+						data.forEach(client => {
+							const pastClient = pastClientTemplate.cloneNode(true);
+							const sectionTitle = pastClient.querySelector(".section_title")
+							const sectionSubscribers = pastClient.querySelector(".past_client_subscribers")
+							const sectionImage = pastClient.querySelector(".past_client_img")
+							const sectionButton = pastClient.querySelector(".past_client_link")
+
+							pastClients.appendChild(pastClient);
+							sectionTitle.innerText = client.username
+							sectionSubscribers.innerText = client.subscribers
+							sectionImage.src = client.profilePicture
+							sectionButton.innerText = client.linkText
+							sectionButton.href = client.linkURL
+						})
+					})
+					.catch(error => {
+						console.error('Error fetching past clients:', error);
+						alert("Failed to retrieve past client data, please try again later.");
+					});
+			} else {
+				const section = template.cloneNode(true);
+				const sectionTitle = section.querySelector(".section_title")
+				const sectionDescription = section.querySelector(".section_description")
+				const iframeContainer = section.querySelector(".iframe_container")
+				
+				sections.appendChild(section);
+				sectionTitle.innerText = item.title
+				sectionDescription.innerText = item.description
+				
+				if (item.hasCoolFont == true) {
+					sectionTitle.classList.add("coolFont");
 				}
-				iframeContainer.appendChild(iframe)
-			})
+				if (item.color != undefined) {
+					section.style["background-color"] = item.color;
+				}
+				if (item.descriptionColor != undefined) {
+					sectionDescription.style.color = item.descriptionColor;
+				}
+	
+				let x = 789;
+				let y = 442;
+	
+				if (item.videoLinks.length > 1) {
+					x = 640;
+					y = 360;
+				}
+				item.videoLinks.forEach(link => {
+					const iframe = document.createElement("iframe")
+					iframe.src = link[0]
+					iframe.setAttribute("frameBorder", "0")
+					iframe.setAttribute("allowfullscreen", "")
+					if (link[1] == "VIDEO") {
+						iframe.width = x
+						iframe.height = y
+					} else if (link[1] == "SHORT") {
+						iframe.width = y
+						iframe.height = x
+					}
+					iframeContainer.appendChild(iframe)
+				})
+			}
 		});
 
 	})
@@ -59,7 +102,6 @@ fetch('/static/json/videos.json')
 		console.error('Error fetching videos:', error);
 		alert("Failed to retrieve video data, please try again later.");
 	});
-
 
 // Mobile Support & Overflow Detection
 const header = document.getElementById('header');
@@ -107,3 +149,13 @@ document.addEventListener('DOMContentLoaded', updateClassesBasedOnOverflow);
 // Attach the function to the window resize and orientationchange events
 window.addEventListener('resize', updateClassesBasedOnOverflow);
 window.addEventListener('orientationchange', updateClassesBasedOnOverflow);
+
+setTimeout(() => {
+	updateClassesBasedOnOverflow();
+}, 1000);
+setTimeout(() => {
+	updateClassesBasedOnOverflow();
+}, 2000);
+setTimeout(() => {
+	updateClassesBasedOnOverflow();
+}, 3000);
